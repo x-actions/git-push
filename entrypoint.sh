b479757f1ec784db20dc56f3540e9d5a14c18e0b
xiexianbin <me@xiexianbin.cn>
2020-01-24 22:38:49 +0800

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,6 +28,8 @@ mkdir /root/.ssh
 ssh-keyscan -t rsa github.com > /root/.ssh/known_hosts
 echo ${DEPLOY_PRIVATE_KEY} > /root/.ssh/id_rsa
 chmod 400 /root/.ssh/id_rsa
+ls -lhart /root/.ssh/id_rsa
+cat /root/.ssh/id_rsa
 
 echo "## Push to Github ##################"
 rm -rf .git
